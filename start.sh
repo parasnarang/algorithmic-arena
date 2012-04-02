00d5d9a340c5d5660f681483c9096f7e9928ee9d
Jai <jai.gupta@cse.iitkgp.ernet.in>
2012-04-02 22:49:20 +0530

    adding arena creator

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 if [ $# -lt 1 ]
 then
-    echo "Enter new program name"
+    echo "No Program name specified"
     exit 1
 fi
 if [ ! -e $1 ]
 then
-mv *.cpp ../doneprogs/
-cp src/template.cpp $1.cpp
-rm -f input output
-emacs -nw input output src/myalgo.h $1.cpp
+    mv *.cpp ../doneprogs/
+    rm -f input output
+    cp ../src/template.cpp $1.cpp
+    emacs -nw input output ../src/myalgo.h $1.cpp
 else
-emacs -nw input output src/myalgo.h $1
+    emacs -nw input output ../src/myalgo.h $1
 fi
 
